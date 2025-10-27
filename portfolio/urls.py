8cbdaa57c7cb4723c9c6f88dd894b6625e84fe5d
CodeByNilesh <nj685653@gmail.com>
2025-10-27 07:39:52 +0530

    Emergency admin reset

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,10 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('projects/', views.projects, name='projects'),
-    path('certifications/', views.certifications, name='certifications'),
-    path('contact/', views.contact, name='contact'),
     path('project/<slug:slug>/', views.project_detail, name='project_detail'),
-    
-    # EMERGENCY - DELETE AFTER USE
-    path('reset-admin-emergency/', views.reset_admin_now),
+    path('certifications/', views.certifications, name='certifications'),  # NEW
+    path('contact/', views.contact, name='contact')
 ]
